b74d10efd6fdaa5ece02b0056c581e1bab951cb5
kshithij <kshithijy@gmail.com>
2025-05-08 15:06:06 +0530

    demo sidebar

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,66 @@
+import { Calendar, Home, Inbox, Search, Settings, ChartNoAxesCombined, Target, FileText } from "lucide-react"
+
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+    {
+        title: "Home",
+        url: "#",
+        icon: Home,
+    },
+    {
+        title: "Simulation",
+        url: "#",
+        icon: ChartNoAxesCombined,
+    },
+    {
+        title: "Challenges",
+        url: "#",
+        icon: Target,
+    },
+    {
+        title: "Feedback",
+        url: "#",
+        icon: FileText,
+    },
+]
+
+export function AppSidebar() {
+    return (
+        <Sidebar collapsible="icon">
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Application</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map((item) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <a href={item.url}>
+                                            <item.icon />
+                                            <span>{item.title}</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter>
+                
+            </SidebarFooter>
+        </Sidebar>
+    )
+}
